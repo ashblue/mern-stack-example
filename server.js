4ad9be81e87dcf1804402c03aa2170615a3e3e23
Ash Blue <ash@blueashes.com>
2019-08-07 15:34:47 -0600

    Added user creation

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 app.use('/api/users', require('./routes/api/users'));
