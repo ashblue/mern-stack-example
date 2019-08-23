96f1e809aaa9d614dfe5ed646e8e7cd6eb8d61ed
Ash Blue <ash@blueashes.com>
2019-08-23 17:17:26 -0600

    Added actions and reducers to handle registration

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,8 +3,9 @@ import {connect} from 'react-redux';
 import {Link} from "react-router-dom";
 import {setAlert} from "../../actions/alert";
 import PropTypes from 'prop-types';
+import {register} from "../../actions/auth";
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register}) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -24,7 +25,7 @@ const Register = ({setAlert}) => {
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('success');
+      register({name, email, password})
     }
   };
 
@@ -91,6 +92,13 @@ const Register = ({setAlert}) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
 
-export default connect(null, {setAlert})(Register);
+export default connect(
+  null,
+  {
+    setAlert,
+    register,
+  },
+  )(Register);
