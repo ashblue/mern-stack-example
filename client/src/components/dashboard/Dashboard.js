5c16a97197d61a003f0177bfa90ca4595838534f
Ash Blue <ash@blueashes.com>
2019-08-26 15:11:07 -0600

    Dashboard now detects if a profile is present or not

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,17 +1,48 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, Fragment} from 'react';
+import {Link} from "react-router-dom";
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {getCurrentProfile} from "../../actions/profile";
+import Spinner from "../layout/Spinner";
 
-const Dashboard = ({getCurrentProfile, auth, profile}) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: {user},
+  profile: {profile, loading},
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
 
+  function displayProfile() {
+    if (profile !== null) {
+      return <Fragment>has</Fragment>;
+    }
+
+    return <Fragment>
+      <p>You have not setup a profile, please add some info</p>
+      <Link to='/create-profile' className='btn btn-primary my-1'>
+        Create Profile
+      </Link>
+    </Fragment>;
+  }
+
+  if (loading && profile === null) {
+    return <Spinner />;
+  }
+
   return (
-    <div>
-      Dashboard
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">
+        Dashboard
+      </h1>
+
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome {user && user.name}
+      </p>
+
+      {displayProfile()}
+    </Fragment>
   );
 };
 
