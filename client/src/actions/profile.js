03c0b562444a93296806108d4d690e31eb9f176a
Ash Blue <ash@blueashes.com>
2019-08-27 17:42:50 -0600

    Added ability to create a profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,6 +4,7 @@ import {
   GET_PROFILE,
   PROFILE_ERROR,
 } from "./types";
+import {setAlert} from "./alert";
 
 export const getCurrentProfile = () => async dispatch => {
   try {
@@ -13,6 +14,42 @@ export const getCurrentProfile = () => async dispatch => {
       payload: res.data,
     });
   } catch (e) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: e.response.statusText,
+        status: e.response.status,
+      }
+    });
+  }
+};
+
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (e) {
+    const errors = e.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+    }
+
     dispatch({
       type: PROFILE_ERROR,
       payload: {
