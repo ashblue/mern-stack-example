f8c3ee35fd08a2d7a5ca7a988e4f17dd72b4aeef
Ash Blue <ash@blueashes.com>
2019-09-05 15:51:01 -0600

    Added social link printing

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import {connect} from 'react-redux';
 import {getProfileById} from "../../actions/profile";
 import Spinner from "../layout/Spinner";
 import {Link} from "react-router-dom";
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 
 const Profile = (
   {
@@ -31,8 +33,14 @@ const Profile = (
               auth.isAuthenticated
               && auth.loading === false
               && auth.user._id === profile.user._id
-              && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)
+              && (<Link to='/edit-profile'
+                        className='btn btn-dark'>Edit Profile</Link>)
             }
+
+            <div className="profile-grid my-1">
+              <ProfileTop profile={profile} />
+              <ProfileAbout profile={profile} />
+            </div>
           </Fragment>
       }
     </Fragment>
---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,45 @@
+import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileAbout = (
+  {
+    profile: {
+      bio,
+      skills,
+      user: {
+        name,
+      },
+    },
+  },
+) => {
+  function printSkills () {
+    return skills.map((skill, index) => (
+      <div key={index} className='p-1'>
+        <i className="fas fa-check"></i> {skill}
+      </div>
+    ));
+  }
+
+  return (
+    <div className="profile-about bg-light p-2">
+      {bio && (
+        <Fragment>
+          <h2 className="text-primary">{name.split(' ')[0]}s Bio</h2>
+          <p>{bio}</p>
+          <div className="line"/>
+        </Fragment>
+      )}
+
+      <h2 className="text-primary">Skill Set</h2>
+      <div className="skills">
+        {printSkills()}
+      </div>
+    </div>
+  );
+};
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
+
+export default ProfileAbout;
